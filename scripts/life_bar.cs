1ba67f2fdaacffd4165c0046d0b7ec1532d81015
MrOnosa <supersnapple@gmail.com>
2024-06-29 19:57:25 -0500

    Life bar - Replaced grid with sprites

--- scripts/life_bar.cs
+++ scripts/life_bar.cs
@@ -1,42 +1,69 @@
 using Godot;
 using System;
+using System.Linq;
 
 public partial class life_bar : Node2D
 {
-    private const int TileIndex = 1;
+    private const int Offset = 34;
+    [Export] public PackedScene HeartScene { get; set; }
 
-    private TileMap _tileMap;
+    private int _hp;
+    private double _wiggleCount = 0.05;
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
-        _tileMap = GetNode<TileMap>("TileMap");
         Paint(10);
-    }
-
-    public void Paint(int hp)
-    {
-        GD.Print("Setting life bar to "+hp);
-        
-        for (int i = 0; i < 5; i++)
-        {
-            //Clear
-            _tileMap.SetCell(0, new Vector2I(i, 0), TileIndex);
-        }
-        var odd = hp % 2 == 1;
-        for (int i = 0; i < hp / 2; i++)
-        {
-            _tileMap.SetCell(0, new Vector2I(i, 0), TileIndex, Vector2I.Zero);
-        }
-
-        if (odd)
-        {
-            _tileMap.SetCell(0, new Vector2I((hp / 2), 0), TileIndex, new Vector2I(1, 0));
-        }
     }
 
     // Called every frame. 'delta' is the elapsed time since the previous frame.
     public override void _Process(double delta)
     {
+        _wiggleCount -= delta;
+        if (_wiggleCount < 0)
+        {
+            _wiggleCount = 0.05;
+            if (_hp < 5)
+            {
+                var x = 0;
+                foreach (var child in GetChildren().OfType<AnimatedSprite2D>().ToList())
+                {
+                    child.Position = new Vector2(x + GD.RandRange(-1,1) , GD.RandRange(-1,1));
+                    x += Offset;
+                }
+            }
+        }
+    }
+
+    public void Paint(int hp)
+    {
+        _hp = hp;
+        GD.Print("Setting life bar to "+hp);
+        
+        for (int i = 0; i < 5; i++)
+        {
+            foreach (var child in GetChildren())
+            {
+                child.QueueFree();
+            }
+        }
+
+        var x = 0;
+        var odd = hp % 2 == 1;
+        for (int i = 0; i < hp / 2; i++)
+        {
+            var heart = HeartScene.Instantiate<AnimatedSprite2D>();
+            heart.Position = new Vector2(x, 0);
+            x += Offset;
+            AddChild(heart);
+        }
+
+        if (odd)
+        {
+            var heart = HeartScene.Instantiate<AnimatedSprite2D>();
+            heart.Position = new Vector2(x, 0);
+            heart.Frame = 1;
+            AddChild(heart);
+        }
     }
 }
